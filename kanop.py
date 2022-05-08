43409e70cfc5fbfc117a4c7586802c407b43906e
Jean-Philippe NOËL <jpnoel@pledg.co>
2022-05-08 16:45:23 +0200

    Display a map

--- kanop.py
+++ kanop.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import folium
 
 app = Flask(__name__)
 
+KANOP_COORDS = (48.83505261412694, 2.37097587670811)
+
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    folium_map = folium.Map(location=KANOP_COORDS, zoom_start=14)
+    folium.Marker(
+        location=KANOP_COORDS,
+        popup="Kanop",
+    ).add_to(folium_map)
+    return folium_map._repr_html_()
