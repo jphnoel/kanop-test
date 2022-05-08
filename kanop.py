bf8c0695940b567b86f4db1df13ba27fbe90a304
Jean-Philippe NOËL <jpnoel@pledg.co>
2022-05-08 18:31:40 +0200

    Manage the users authentication

--- kanop.py
+++ kanop.py
@@ -1,12 +1,75 @@
+import os
+
 from flask import Flask
+from flask_security import (
+    Security,
+    SQLAlchemyUserDatastore,
+    auth_required,
+    hash_password,
+)
+from flask_security.models import fsqla_v2 as fsqla
+from flask_sqlalchemy import SQLAlchemy
+
 import folium
 
-app = Flask(__name__)
-
 KANOP_COORDS = (48.83505261412694, 2.37097587670811)
+EMAIL = "test@me.com"
+PASSWORD = "password"
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+# Generate a nice key using secrets.token_urlsafe()
+app.config["SECRET_KEY"] = os.environ.get(
+    "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
+)
+# Bcrypt is set as default SECURITY_PASSWORD_HASH, which requires a salt
+# Generate a good salt using: secrets.SystemRandom().getrandbits(128)
+app.config["SECURITY_PASSWORD_SALT"] = os.environ.get(
+    "SECURITY_PASSWORD_SALT", "146585145368132386173505678016728509634"
+)
+
+# Use an in-memory db
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+# As of Flask-SQLAlchemy 2.4.0 it is easy to pass in options directly to the
+# underlying engine. This option makes sure that DB connections from the
+# pool are still valid. Important for entire application since
+# many DBaaS options automatically close idle connections.
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "pool_pre_ping": True,
+}
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+# Define models
+fsqla.FsModels.set_db_info(db)
+
+
+class Role(db.Model, fsqla.FsRoleMixin):
+    pass
+
+
+class User(db.Model, fsqla.FsUserMixin):
+    pass
+
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    if not user_datastore.find_user(email=EMAIL):
+        user_datastore.create_user(email=EMAIL, password=hash_password(PASSWORD))
+    db.session.commit()
 
 
 @app.route("/")
+@auth_required()
 def index():
     folium_map = folium.Map(location=KANOP_COORDS, zoom_start=14)
     folium.Marker(
